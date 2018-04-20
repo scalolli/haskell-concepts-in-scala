d26cc8f252eb859e7c67be405cd7a6ce19984452
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-04-20 07:11:52 +0100

    now I know what =:= means, crazy

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -7,14 +7,28 @@ object Runner extends App {
   val Fname = "abc"
   Fname.equals()
 
+  // The reason to have a Tag is to do assertions where we have access to the type for e.g below
+  // we cannot pass to foo(Integer(1)) but foo(Long(1)) works
   type Tagged[U] = {
       type Tag = U
   }
 
   println("Hello")
 
-  val x: Tagged[Long] = foo(Long(1))
+  val x = 1.asInstanceOf[Tagged[Long]]
+  foo(x)
 
-  def foo[T](a: Tagged[T])(implicit ev: a.Tag = Long) : Tagged[Long] = a.asInstanceOf[Tagged[Long]]
+//  val y: Tagged[Long] = foo(1)
+
+  def foo[T](a: Tagged[T])(implicit ev: a.Tag =:= Long) : Tagged[Long] = a.asInstanceOf[Tagged[Long]]
+
+  type @@[A, T] = A with Tagged[T]
+
+  object Tag {
+    def Tag[A, T](a: A) = a.asInstanceOf[@@[A, T]]
+  }
+
+  sealed trait Multiplication
+
 
 }
