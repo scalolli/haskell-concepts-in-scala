83fc4b37fb4137b7892b8042c0fb5b25eebec7dd
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-04-20 07:28:28 +0100

    trying to write my Monoid for new type multiplication

---
+++ src/main/scala/ScalaNewTypes.scala
@@ -0,0 +1,43 @@
+
+
+object ScalaNewTypes extends App {
+
+  type FName = String
+
+  val Fname = "abc"
+  Fname.equals()
+
+  // The reason to have a Tag is to do assertions where we have access to the type for e.g below
+  // we cannot pass to foo(Integer(1)) but foo(Long(1)) works
+  type Tagged[U] = {
+    type Tag = U
+  }
+
+  println("Hello")
+
+  val x = 1.asInstanceOf[Tagged[Long]]
+  foo(x)
+
+  //  this wont work
+  //  val y: Tagged[Long] = foo(1)
+
+  def foo[T](a: Tagged[T])(implicit ev: a.Tag =:= Long): Tagged[Long] = a.asInstanceOf[Tagged[Long]]
+
+  type @@[A, T] = A with Tagged[T]
+
+  object Tag {
+    def apply[A, T](a: A) = a.asInstanceOf[@@[A, T]]
+  }
+
+  sealed trait Multiplication
+
+  private val oneMultiplicationNewTypeForInt: @@[Int, Multiplication] = Tag[Int, Multiplication](2)
+  private val twoMultiplicationNewTypeForInt: @@[Int, Multiplication] = Tag[Int, Multiplication](2)
+
+  val result = |+|(oneMultiplicationNewTypeForInt, twoMultiplicationNewTypeForInt)
+
+  def |+|(a: @@[Int, Multiplication], b: @@[Int, Multiplication]): @@[Int, Multiplication] = Tag(a * b)
+
+  print(s"Its still just a int: $result")
+
+}
