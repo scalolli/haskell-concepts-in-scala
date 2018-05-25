5112d2df9475b1a6c1ad7a3c4910e1916e6a65bd
scalolli <basavarajkalloli@gmail.com>
2018-05-25 18:02:25 +0100

    implementing a semigroup instance for Four a b c d from haskell book

--- src/main/scala/haskell/SemigroupExercises.scala
+++ src/main/scala/haskell/SemigroupExercises.scala
@@ -1,17 +1,36 @@
 package haskell
 
-import cats.kernel.Semigroup
 import cats.implicits._
+import cats.kernel.Semigroup
 
 object SemigroupExercises extends App {
 
-  case class Four(a: Int)
+  case class IntegerWrapper(a: Int) extends AnyVal
 
-  object Four {
-    implicit object CustomSemiGroup extends Semigroup[Four] {
-      override def combine(x: Four, y: Four): Four = Four(x.a + y.a)
-    }
+  object IntegerWrapper {
+    implicit def integerWrapperInstance(implicit intInstance: Semigroup[Int]): Semigroup[IntegerWrapper] =
+      (a: IntegerWrapper, b: IntegerWrapper) => IntegerWrapper(intInstance.combine(a.a, b.a))
   }
 
-  Four(1) |+| Four(2)
+  IntegerWrapper(1) |+| IntegerWrapper(2)
+
+  case class Four[A, B, C, D](a: A, b: B, c: C, d: D)
+
+  object Four {
+
+    implicit def fourSemigroupInstance[A, B, C, D](implicit A: Semigroup[A],
+                                                   B: Semigroup[B],
+                                                   C: Semigroup[C],
+                                                   D: Semigroup[D]): Semigroup[Four[A, B, C, D]] =
+      (x: Four[A, B, C, D], y: Four[A, B, C, D]) =>
+        Four(
+          A.combine(x.a, y.a),
+          B.combine(x.b, y.b),
+          C.combine(x.c, y.c),
+          D.combine(x.d, y.d)
+      )
+  }
+
+  Four(1, 2, 3, 4) |+| Four(2, 3, 4, 5)
+  Four("1", "2", "3", "4") |+| Four("2", "3", "4", "5")
 }
