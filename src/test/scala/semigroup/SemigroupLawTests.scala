5730d041d09f8fce9f23bdcb4bc7c8c9451f7d14
Basavaraj Kalloli <basavaraj.kalloli@gmail.com>
2018-05-30 06:57:08 +0100

    todo for writing applicative syntax for arbitrary

--- src/test/scala/semigroup/SemigroupLawTests.scala
+++ src/test/scala/semigroup/SemigroupLawTests.scala
@@ -8,6 +8,19 @@ import org.scalacheck.{Arbitrary, Gen}
 
 class SemigroupLawTests extends CatsSuite {
 
+//  TODO: how do we get this to compile ?? Ask in Cats channel?
+//  implicit def functorGen: Functor[Gen] = new Functor[Gen] {
+//    override def map[A, B](fa: Gen[A])(f: A => B): Gen[B] = fa.map(f)
+//  }
+//
+//  implicit def semigroupGen[A](implicit sA: Semigroup[A]): Semigroup[Gen[A]] = new Semigroup[A] {
+//    override def combine(x: Gen[A], y: Gen[A]): A = sA.combine()
+//  }
+//
+//  implicit def arbFour[A: Arbitrary, B: Arbitrary, C: Arbitrary, D: Arbitrary]: Arbitrary[Four[A, B, C, D]] =
+//    Arbitrary((Arbitrary.arbitrary[A], Arbitrary.arbitrary[B], Arbitrary.arbitrary[C], Arbitrary.arbitrary[D]).mapN(Four.apply))
+
+
   implicit def eqFour[A: Eq, B: Eq, C: Eq, D: Eq]: Eq[Four[A, B, C, D]] = Eq.fromUniversalEquals
   implicit def arbFour[A: Arbitrary, B: Arbitrary, C: Arbitrary, D: Arbitrary]: Arbitrary[Four[A, B, C, D]] =
     Arbitrary(
